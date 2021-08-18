a55b660a9784d01d632dfc31f33b87b18efba49c
Prince Okumo <prokum@ttu.ee>
2021-08-18 19:26:59 +0300

    first f

---
+++ Complex.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: admin
+ * Date: 8/18/2021
+ * Time: 10:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PAR_IQ
+{
+	/// <summary>
+	/// Description of Complex.
+	/// </summary>
+	public class Complex
+	{
+		public Complex()
+		{
+		}
+	}
+}
