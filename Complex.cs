fa7729c32d917fbf7f031b66a30140bfb22e8195
Prince Okumo <prokum@ttu.ee>
2021-08-18 19:41:26 +0300

    update

--- Complex.cs
+++ Complex.cs
@@ -7,16 +7,44 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace PAR_IQ
 {
 	/// <summary>
 	/// Description of Complex.
+	/// this class will define the list parameters for IQ inputs
 	/// </summary>
+	/// 
+	
+	
 	public class Complex
 	{
-		public Complex()
+		public double _inter = 0;
+		public double _quad =0 ;
+		public double prap =0 ;
+		public Complex(string Inter, string Quad)
 		{
+			_inter = Convert.ToDouble(Inter);
+			_quad =Convert.ToDouble(Quad);
+			
 		}
+		 double ParCalculation (List<Complex> inputIQ)
+		{
+
+		 	List <Complex>input= new List<Complex>();
+		 	//foreach (Complex value in inputIQ)
+		 		_inter *= _inter;
+		 		_quad *= _quad;
+		 	//input.Add(new Complex (Convert.ToDouble_inter,Convert.ToDouble_quad));
+		 	//double II = Math.Pow(_inter,2) ;
+		 	return prap;
+		}
+			
+		
+		
+		
 	}
 }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,38 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+	
 namespace PAR_IQ
 {
 	class Program
 	{
+		
+		static List<Complex> inputIQ = new List<Complex>();
+		
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
 			// TODO: Implement Functionality Here
+			//List<Complex> inputIQ = new List<Complex>();
+			Console.WriteLine("Please enter an inter");
+			string inter = Console.ReadLine();
+			
+			Console.WriteLine("Please enter an Q");
+			string quad = Console.ReadLine();
+			
+			inputIQ.Add(new Complex (inter, quad));
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
+		
+		
+		
+		
 	}
+	
 }
