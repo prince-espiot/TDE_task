d80773281495e535668fd56dced13f1162bdbc16
Prince Okumo <prokum@ttu.ee>
2021-08-18 08:41:35 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: admin
+ * Date: 8/18/2021
+ * Time: 08:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PAR_IQ
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
